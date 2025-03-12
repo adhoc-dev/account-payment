90440439e894d6748cefaf177012525025c5f11c
Camila Vives <cav@adhoc.com.ar>
2025-03-12 14:17:40 -0300

    [FIX] account_cashbox: sum in company currency if journal doesnt have currency

--- account_cashbox/models/account_cashbox_session_line.py
+++ account_cashbox/models/account_cashbox_session_line.py
@@ -30,11 +30,13 @@ class PopSessionJournalControl(models.Model):
             [("cashbox_session_id", "in", self.mapped("cashbox_session_id").ids), ("state", "!=", "draft")]
         )
         for record in self:
-            amount = sum(
-                payments_lines.filtered(
-                    lambda p: p.cashbox_session_id == record.cashbox_session_id and p.journal_id == record.journal_id
-                ).mapped("amount_signed")
+            filtered_lines = payments_lines.filtered(
+                lambda p: p.cashbox_session_id == record.cashbox_session_id and p.journal_id == record.journal_id
             )
+            if record.journal_id.currency_id:
+                amount = sum(filtered_lines.mapped('amount_signed'))
+            else:
+                amount = sum(filtered_lines.mapped('amount_company_currency_signed'))
             record.amount = amount
             record.balance_end = amount + record.balance_start
             self -= record
